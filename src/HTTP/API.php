d4b79f04ddc8f67544471e5343f182e01e273fa1
Manuel Muñoz <manus.reload@gmail.com>
2016-06-10 13:49:43 +0200

    Fixeado function de la API y View

--- src/HTTP/API.php
+++ src/HTTP/API.php
@@ -1,23 +1,5 @@
 <?php
 /**
- *     GLFramework, small web application framework.
- *     Copyright (C) 2016.  Manuel Muñoz Rosa
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
  * Created by PhpStorm.
  * User: manus
  * Date: 25/04/16
@@ -80,4 +62,14 @@ class API
         $url = $this->getURI($uri) . "?" . $fields_string;
         return get($url, $this->getAuth());
     }
+
+    /**
+     * @return array|mixed|null
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    
 }
