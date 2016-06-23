f4221ae92ac16974e248e2c327b86dca2508cc05
Manu Muñoz <manus.reload@gmail.com>
2016-06-23 16:53:20 +0200

    mejora de versiones

--- src/Versions.php
+++ src/Versions.php
@@ -40,4 +40,10 @@ class Versions
         $versions = $this->getVersions();
         return $versions[0];
     }
+
+    public function getVersion($index = 0)
+    {
+        $versions = $this->getVersions();
+        return $versions[$index];
+    }
 }
