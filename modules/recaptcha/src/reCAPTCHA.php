2e808916fedd7d0a313dc79997d1f560b1ecfc90
Manu Muñoz <manus.reload@gmail.com>
2016-09-28 12:15:13 +0200

    plugin recaptcha y log de errores en pantalla

---
+++ modules/recaptcha/src/reCAPTCHA.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GLFramework\Modules\reCAPTCHA;
+/**
+ * Created by PhpStorm.
+ * User: mmuno
+ * Date: 26/09/2016
+ * Time: 17:10
+ */
+class reCAPTCHA
+{
+
+    function displayScripts()
+    {
+        echo "<script src='https://www.google.com/recaptcha/api.js'></script>";
+    }
+}
