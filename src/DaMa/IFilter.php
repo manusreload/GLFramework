072a9b0a84c775e81f1a234ba7ab5678331e24f6
mmunoz <mmunoz@gestionlan.com>
2016-11-23 18:25:10 +0100

    filtro de modelos

---
+++ src/DaMa/IFilter.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mmunoz
+ * Date: 23/11/16
+ * Time: 17:57
+ */
+
+namespace GLFramework\DaMa;
+
+
+interface IFilter
+{
+
+    public function filter($model);
+
+}
