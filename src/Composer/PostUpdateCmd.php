ef7aef4ecdc349620cb2c6b117caa1fd25ce4392
Manu Muñoz <manus.reload@gmail.com>
2017-05-23 13:29:49 +0200

    eventos de instalacion

---
+++ src/Composer/PostUpdateCmd.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manus
+ * Date: 23/5/17
+ * Time: 13:21
+ */
+
+namespace GLFramework\Composer;
+
+
+use Composer\Script\Event;
+
+class PostUpdateCmd
+{
+
+    public static function exec(Event $event)
+    {
+
+    }
+}
